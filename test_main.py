369f2ff052e2e19b62e8090c41b975c5db8504c5
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2024-03-06 03:36:53 -0300

    Init project

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main import descobre_numero
+
+class TesteDescobreNumero(unittest.TestCase):
+    def test_descobre_numero(self):
+        for _ in range(2):  # Executa o teste duas vezes
+            resultado = descobre_numero(5)
+            self.assertTrue(resultado.startswith("O número que você adivinhou foi: "))
+            numero = int(resultado.split(": ")[1])
+            self.assertTrue(descobre_numero == numero, "O programa não adivinhou meu número")
+
+if __name__ == '__main__':
+    unittest.main()
